d1dc95d370d9c434de7e48efde839fbd8fd43e85
NEKiarie <s49188705@gmail.com>
2023-09-27 10:02:52 +0300

    :sparkles:Added AuthForm Feature

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import Logo from "../../public/images/logo .png";
+import Logo from "../../public/images/logo.png";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
     <div
-      className="flex min-h-full flex-col 
-         justify-center py-12 
-         sm:px-6 lg:px-8
+      className="flex
+          min-h-full
+          flex-col 
+          justify-center 
+          py-12 
+          sm:px-6 
+          lg:px-8
          bg-gray-100"
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -17,10 +22,11 @@ export default function Home() {
           width="48"
           className="mx-auto w-auto"
         />
-        <h2 className="">
-
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign In to your Account
         </h2>
       </div>
+      <AuthForm />
     </div>
   );
 }
